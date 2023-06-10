26b87fb21c0afd6ce3e9f2293ec1a1043fbacce0
Toki77777 <135839114+Toki77777@users.noreply.github.com>
2023-06-10 14:23:20 +0900

    add pallet file(can't be loaded...) and assets

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # scaraction
 
-二次創作ゲーム（製作予定）です
+二次創作ゲーム（製作中）です
+
+
+
+
+
+＜使用させていただいたもの＞
+
+・放浪者　ドット絵
+https://www.s-e-project.com/genshin-impact/scaramouche-wanderer-pixelart/
+
--- game/main.py
+++ game/main.py
@@ -3,20 +3,23 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="sucaraction", fps=60)
-        #opening menu
+        #Opening menu
         self.op_menu_1 = "Start"
         self.op_menu_2 = "Load"
         self.op_menu_3 = "Quit"
         self.menu_items = [self.op_menu_1, self.op_menu_2, self.op_menu_3]
         self.menu_selected_item = 0
-        #Player status
+        #User status
         self.mx = 0.0
         self.my = 0.0
         #Map status
         self.game_start = False
         self.stage_flag = 1
-        #read
+        #load
         pyxel.load("my_resource.pyxres")
+        pyxel.image(1).load(0, 0, "assets/genshin-Scaramouche-Wanderer.png")
+        self.scara_W = 32
+        self.scara_H = 32
 
     def run(self):
         pyxel.run(self.update, self.draw)
@@ -29,19 +32,12 @@ class App:
             elif pyxel.btnp(pyxel.KEY_W):
                 self.menu_selected_item = (self.menu_selected_item - 1) % len(self.menu_items)
             elif pyxel.btnp(pyxel.KEY_RETURN):
-                self.handle_menu_selection()
+                self.handle_menu_selection(self)
         else:
-            #ゲーム本編での操作を記述
-            self.game_base()
-
-    def game_base():
-        #ゲーム本編での操作を記述
-        1*1
-
+            #ゲーム本編での操作を実行
+            self.game_main(self)
 
     def draw(self):
-        #ゲーム本編での描画を記述
-        pyxel.cls(0)
         #タイトル画面での描画を記述
         if self.game_start == False:
             pyxel.cls(1)
@@ -50,11 +46,23 @@ class App:
             for i, item in enumerate(self.menu_items):
                 if i == self.menu_selected_item:
                     pyxel.text(58, 65+10*i, "<"+item+">", 7)
+                    pyxel.blt(20, 65+10*i, 1, 0, 0, self.scara_W, self.scara_H, colkey=0)
                 else:
                     pyxel.text(58, 65+10*i, item, 13)
+        else:
+            #ゲーム本編での描画を実行
+            self.game_main_draw(self)
+
+    def game_main(self):
+        #ゲーム本編での操作を記述
+        1*1
+
+    def game_main_draw(self):
+        #ゲーム本編での描画を記述
+        pyxel.cls(0)
 
     def handle_menu_selection(self):
-        #opening menuでEnterを推した時の動作を記述
+        #opening menuでEnterを押した時の動作を記述
         self.menu_selected_item_text = self.menu_items[self.menu_selected_item]
         if self.menu_selected_item_text == self.op_menu_1:
             #はじめから
@@ -65,6 +73,5 @@ class App:
         elif self.menu_selected_item_text == self.op_menu_3:
             pyxel.quit()
             #確認画面を表示してもいいかも
-
 
 App().run()
