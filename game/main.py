665c8f807b861d1a46417a468100dcb8947532cb
Toki77777 <135839114+Toki77777@users.noreply.github.com>
2023-06-10 19:21:59 +0900

    added menu select icon

--- game/main.py
+++ game/main.py
@@ -8,7 +8,9 @@ class App:
         #load PyxelUnicode
         self.font_path = "assets/misaki_gothic_2nd.ttf"
         self.font_size = 8
+        self.font_size_2x = 16
         self.pyuni = PyxelUnicode(self.font_path, self.font_size)
+        self.pyuni_capital = PyxelUnicode(self.font_path, self.font_size_2x)
         #Opening menu
         self.op_menu_1 = "Start"
         self.op_menu_2 = "Load"
@@ -42,16 +44,21 @@ class App:
 
     def draw(self):
         #タイトル画面での描画を記述
+        #ローカル変数定義
+        self.x = 105
+        self.blt_x = 86
+        self.y = 120
+        self.yi = 20
+        #処理
         if self.is_game_start == False:
             pyxel.cls(1)
-            pyxel.blt(28, 30, 0, 0, 32, 96, 16, colkey=1)
             #menu button
             for i, item in enumerate(self.menu_items):
                 if i == self.menu_selected_item:
-                    self.pyuni.text(120, 65+10*i, "<"+item+">")
-                    pyxel.blt(94, 65+10*i, 0, 0, 0, 24, 24)#, colkey=0)
+                    self.pyuni_capital.text(self.x, self.y+self.yi*i, item)
+                    pyxel.blt(self.blt_x, self.y+self.yi*i, 0, 0, 0, 17, 17, colkey=7)
                 else:
-                    self.pyuni.text(120, 65+10*i, item, 13)
+                    self.pyuni_capital.text(self.x, self.y+self.yi*i, item, 13)
         else:
             #ゲーム本編での描画を実行
             self.game_main_draw(self)
